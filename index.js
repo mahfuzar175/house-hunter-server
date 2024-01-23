adece7ba49018d8ffb2e9c73ae8505218172c2e2
mahfuzar175 <mahfuzar175@gmail.com>
2024-01-24 02:42:50 +0600

    create api for jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded.decoded;
+        next();
+    })
+}
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -30,6 +48,12 @@ async function run() {
     const houseCollection = client.db("houseDB").collection("houses");
     const usersCollection = client.db("houseDB").collection("allusers");
 
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        res.send({ token })
+    })
+
     app.post('/allusers', async(req, res) =>{
         const {name, role, phone, email, password} = req.body;
         const existingUser = await usersCollection.findOne({email});
@@ -39,6 +63,20 @@ async function run() {
 
         await usersCollection.insertOne({name, role, phone, email, password});
         return res.status(200).json({message: 'Registration Successfull'});
+    })
+
+    app.post('/login', async(req, res) =>{
+        const {email, password} = req.body;
+        const user = await usersCollection.findOne({email});
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        res.status(200).json({ message: 'Login successfull', user });
     })
 
     app.get('/houses', async(req, res) =>{
